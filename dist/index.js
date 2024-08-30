36ae25c00ce97e51583b82946f844f4bc8cf944a
NehalSurti <nehalsurti9998@gmail.com>
2024-08-30 23:31:37 +0100

    First commit

---
+++ dist/index.js
@@ -0,0 +1,5 @@
+import express from "express";
+import "dotenv/config";
+const app = express();
+const PORT = process.env.PORT || 7000;
+app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
