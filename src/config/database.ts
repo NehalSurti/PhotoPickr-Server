dc5c4aed1c312d42508f7e296a60827d56371f5c
NehalSurti <nehalsurti9998@gmail.com>
2024-09-06 23:54:59 +0100

    updating

---
+++ src/config/database.ts
@@ -0,0 +1,10 @@
+import { PrismaClient } from "@prisma/client";
+
+// Instantiate PrismaClient
+const prisma = new PrismaClient({
+    log: ["error", "query"],
+    errorFormat: "pretty"
+});
+
+// Export the Prisma client instance
+export default prisma;
